1a2231c4f0a4c74a63d05fd53a12fe7ad2229d7c
Dave Longley <dlongley@digitalbazaar.com>
2025-02-11 14:43:16 -0500

    Add pseudonym shake256 tests.

--- test/pseudonym-test-vectors.js
+++ test/pseudonym-test-vectors.js
@@ -1,11 +1,10 @@
 /*!
- * Copyright (c) 2023-2024 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import {BLS12381_SHA256} from './fixtures/pseudonym-sha256.js';
-// FIXME: enable
-//import {BLS12381_SHAKE256} from './fixtures/pseudonym-shake256.js';
+import {BLS12381_SHAKE256} from './fixtures/pseudonym-shake256.js';
 
 export const CIPHERSUITES_TEST_VECTORS = [
   BLS12381_SHA256,
-  //BLS12381_SHAKE256
+  BLS12381_SHAKE256
 ];
