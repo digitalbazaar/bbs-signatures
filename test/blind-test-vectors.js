c6b67e2ed471ca89ff6996e742f5c3102c109e58
Dave Longley <dlongley@digitalbazaar.com>
2025-02-11 14:23:37 -0500

    Update blind BBS shake256 test fixtures.

--- test/blind-test-vectors.js
+++ test/blind-test-vectors.js
@@ -1,10 +1,10 @@
 /*!
- * Copyright (c) 2023-2024 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2023-2025 Digital Bazaar, Inc. All rights reserved.
  */
 import {BLS12381_SHA256} from './fixtures/blind-sha256.js';
 import {BLS12381_SHAKE256} from './fixtures/blind-shake256.js';
 
 export const CIPHERSUITES_TEST_VECTORS = [
-  //BLS12381_SHAKE256,
+  BLS12381_SHAKE256,
   BLS12381_SHA256
 ];
